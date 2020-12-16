763fd7babd720b61a5f65dc374d366926a4eebd8
An Trinh <atrinh@gmail.com>
2020-12-16 19:16:06 +0000

    Add a11y 🗣

--- scrumdinger/scrumdinger/Views/CardView.swift
+++ scrumdinger/scrumdinger/Views/CardView.swift
@@ -17,9 +17,17 @@ struct CardView: View {
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityElement(children: .ignore)
+                    .accessibilityLabel(Text("Attendees"))
+                    .accessibilityValue(Text("\(scrum.attendees.count)"))
+                
                 Spacer()
+                
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .padding(.trailing, 20)
+                    .accessibilityElement(children: .ignore)
+                    .accessibilityLabel(Text("Meeting length"))
+                    .accessibilityValue(Text("\(scrum.lengthInMinutes) minutes"))
             }
             .font(.caption)
         }
