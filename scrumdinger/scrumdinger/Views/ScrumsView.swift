0742dfbd842875f33f6bbde7046343ee6d05e0f2
An Trinh <atrinh@gmail.com>
2020-12-20 16:53:12 +0000

    Add ScrumsView list showing scrums Make DailyScrum identifiable

---
+++ scrumdinger/scrumdinger/Views/ScrumsView.swift
@@ -0,0 +1,27 @@
+//
+//  ScrumsView.swift
+//  scrumdinger
+//
+//  Created by An Trinh on 20/12/20.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
--- scrumdinger/scrumdinger/scrumdingerApp.swift
+++ scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct scrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            ScrumsView(scrums: DailyScrum.data)
         }
     }
 }
