46a0904a54164c7585ed8ad32c352363cd36d109
An Trinh <atrinh@gmail.com>
2020-12-21 00:50:27 +0000

    Add navigation 🗺

--- scrumdinger/scrumdinger/Views/ScrumsView.swift
+++ scrumdinger/scrumdinger/Views/ScrumsView.swift
@@ -13,15 +13,23 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.color)
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
             }
         }
+        .navigationTitle("Daily Scrums")
+        .navigationBarItems(trailing: Button(action: {}) {
+            Image(systemName: "plus")
+        })
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.data)
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        }
     }
 }
--- scrumdinger/scrumdinger/scrumdingerApp.swift
+++ scrumdinger/scrumdinger/scrumdingerApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct scrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: DailyScrum.data)
+            NavigationView {
+                ScrumsView(scrums: DailyScrum.data)
+            }
         }
     }
 }
