b3a197c05cdce2ab850e2ed0090a2d92310f0f6f
An Trinh <atrinh@gmail.com>
2020-12-24 11:54:18 +0000

    Add edit view

--- scrumdinger/scrumdinger/Views/DetailView.swift
+++ scrumdinger/scrumdinger/Views/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    @State private var isPresented = false
     
     var body: some View {
         List {
@@ -42,7 +43,21 @@ struct DetailView: View {
             }
         }
         .listStyle(InsetGroupedListStyle())
+        .navigationBarItems(trailing: Button("Edit") {
+            isPresented = true
+        })
         .navigationTitle(scrum.title)
+        .fullScreenCover(isPresented: $isPresented) {
+            NavigationView {
+                EditView()
+                    .navigationTitle(scrum.title)
+                    .navigationBarItems(leading: Button("Cancel") {
+                        isPresented = false
+                    }, trailing: Button("Done") {
+                        isPresented = false
+                    })
+            }
+        }
     }
 }
 
