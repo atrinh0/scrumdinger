0aa9f9fdac232e1feee1c75903392bca4d3040b8
An Trinh <atrinh@gmail.com>
2020-12-26 13:28:50 +0000

    Link edit view to create a new scrum

--- scrumdinger/scrumdinger/Views/ScrumsView.swift
+++ scrumdinger/scrumdinger/Views/ScrumsView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @State private var isPresented = false
+    @State private var newScrumData = DailyScrum.Data()
     
     var body: some View {
         List {
@@ -20,9 +22,23 @@ struct ScrumsView: View {
             }
         }
         .navigationTitle("Daily Scrums")
-        .navigationBarItems(trailing: Button(action: {}) {
+        .navigationBarItems(trailing: Button(action: {
+            isPresented = true
+        }) {
             Image(systemName: "plus")
         })
+        .sheet(isPresented: $isPresented) {
+            NavigationView {
+                EditView(scrumData: $newScrumData)
+                    .navigationBarItems(leading: Button("Dismiss") {
+                        isPresented = false
+                    }, trailing: Button("Add") {
+                        let newScrum = DailyScrum(title: newScrumData.title, attendees: newScrumData.attendees, lengthInMinutes: Int(newScrumData.lengthInMinutes), color: newScrumData.color)
+                        scrums.append(newScrum)
+                        isPresented = false
+                    })
+            }
+        }
     }
     
     private func binding(for scrum: DailyScrum) -> Binding<DailyScrum> {
