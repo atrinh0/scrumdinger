376d7b22645a9032337babe15b15769b44ba3df7
An Trinh <atrinh@gmail.com>
2020-12-26 13:59:44 +0000

    Add history section to detail view

--- scrumdinger/scrumdinger/Views/MeetingView.swift
+++ scrumdinger/scrumdinger/Views/MeetingView.swift
@@ -37,6 +37,8 @@ struct MeetingView: View {
         }
         .onDisappear {
             scrumTimer.stopScrum()
+            let newHistory = History(attendees: scrum.attendees, lengthInMinutes: scrumTimer.secondsElapsed/60)
+            scrum.history.insert(newHistory, at: 0)
         }
     }
 }
